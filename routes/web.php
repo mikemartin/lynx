3c6dc06cc31d33880b11d5c1ff357fed955da6a3
Mike Martin <hello@mike-martin.ca>
2020-04-02 00:23:53 +1100

    Fix controller ref

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Statamic\Facades\OAuth;
+use Mikemartin\Bitlynx\Http\Controllers\OAuthController;
 
 Route::name('bitlynx.')->group(function () {
-  Route::get(config('bitlynx.oauth.routes.login'), 'OAuthController@redirectToProvider')->name('oauth.login');
-  Route::get(config('bitlynx.oauth.routes.callback'), 'OAuthController@handleProviderCallback')->name('oauth.callback');
+  Route::get(config('bitlynx.oauth.routes.login'), 'Http\Controllers\OAuthController@redirectToProvider')->name('oauth.login');
+  Route::get(config('bitlynx.oauth.routes.callback'), 'Http\Controllers\OAuthController@handleProviderCallback')->name('oauth.callback');
 });
 
 
