771cf1c0448ccf1049158976e090778b13dd97eb
Mike Martin <hello@mike-martin.ca>
2020-04-02 12:06:31 +1100

    Provider config name

--- config/bitlynx/oauth.php
+++ config/bitlynx/oauth.php
@@ -6,10 +6,10 @@ return [
     'enabled' => true,
 
     'providers' => [
-      \SocialiteProviders\Manager\ServiceProvider::class
+      'bitly'
     ],
 
-    'bit.ly' => [
+    'bitly' => [
       'client_id' => env('BITLY_CLIENT_ID'),
       'client_secret' => env('BITLY_CLIENT_SECRET'),
       'redirect' => env('APP_URL').'/oauth/bitly/callback',
